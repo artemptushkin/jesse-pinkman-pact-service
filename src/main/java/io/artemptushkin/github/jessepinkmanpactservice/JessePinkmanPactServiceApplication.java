b7bde39032131e61ab914464fa5d8ad24284f889
Artem Ptushkin <artem.ptushkin@ev-box.com>
2020-06-30 16:42:16 +0300

    Initial commit

---
+++ src/main/java/io/artemptushkin/github/jessepinkmanpactservice/JessePinkmanPactServiceApplication.java
@@ -0,0 +1,13 @@
+package io.artemptushkin.github.jessepinkmanpactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessePinkmanPactServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JessePinkmanPactServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artemptushkin/github/jessepinkmanpactservice/JessePinkmanPactServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artemptushkin.github.jessepinkmanpactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JessePinkmanPactServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
