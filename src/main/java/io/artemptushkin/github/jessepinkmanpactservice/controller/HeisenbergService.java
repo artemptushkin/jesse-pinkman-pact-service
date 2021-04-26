cdaabc96cc20a9912224037f49c64c565571d704
artemptushkin <artem.ptushkin@ev-box.com>
2021-04-26 09:23:31 +0200

    before consumer scenario 1

--- src/main/java/io/artemptushkin/github/jessepinkmanpactservice/controller/HeisenbergService.java
+++ src/main/java/io/artemptushkin/github/jessepinkmanpactservice/controller/HeisenbergService.java
@@ -2,7 +2,6 @@ package io.artemptushkin.github.jessepinkmanpactservice.controller;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,8 +11,6 @@ public class HeisenbergService {
 	private final RestTemplate restTemplate;
 
 	public HeisenbergResponse cookCrystals(Integer amount) {
-		ResponseEntity<HeisenbergResponse> heisenbergResponseEntity = restTemplate
-				.getForEntity("http://localhost:8091/heisenberg/v1/crystals?amount={0}", HeisenbergResponse.class, amount);
-		return heisenbergResponseEntity.getBody();
+		return null;
 	}
 }
